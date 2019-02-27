b9178225ed995666ce07695f5a32ad3d5056cac3
Henrique Pureza <henrique.pureza00@gmail.com>
2019-02-27 15:03:57 +1100

    Added validation to jwtHelper

--- backend/util/jwtHelper.js
+++ backend/util/jwtHelper.js
@@ -1,6 +1,9 @@
 let jwt = require('jsonwebtoken');
 const SECRET = 'theworldisfullofsecrets';
 
+import AuthController from '../controllers/auth.controller';
+import SessionModel from '../models/sessions.model';
+
 export function generateJWT(req, user){
   const payload = {
       userId: user.id,
@@ -9,44 +12,36 @@ export function generateJWT(req, user){
 
   return jwt.sign(payload, SECRET);
 };
-// class HandlerGenerator {
-//     login (req, res) {
-//       let username = req.body.username;
-//       let password = req.body.password;
-//       // For the given username fetch user from DB
-//       let mockedUsername = 'admin';
-//       let mockedPassword = 'password';
-  
-//       if (username && password) {
-//         if (username === mockedUsername && password === mockedPassword) {
-//           let token = jwt.sign({username: username},
-//             SECRET,
-//             { expiresIn: '24h' // expires in 24 hours
-//             }
-//           );
-//           // return the JWT token for the future API calls
-//           res.json({
-//             success: true,
-//             message: 'Authentication successful!',
-//             token: token
-//           });
-//         } else {
-//           res.send(403).json({
-//             success: false,
-//             message: 'Incorrect username or password'
-//           });
-//         }
-//       } else {
-//         res.send(400).json({
-//           success: false,
-//           message: 'Authentication failed! Please check the request'
-//         });
-//       }
-//     }
-//     index (req, res) {
-//         res.json({
-//           success: true,
-//           message: 'Index page'
-//         });
-//       }
-//     }
+
+export async function validate(req){
+  const token = returnToken(req);
+
+  if (!token) {
+      return Promise.reject(new DuciError(errorTypes.unauthorised.missingToken));
+  }
+
+  const session = await SessionModel.duciFindOne(req, {token: token}, {},
+      {path: 'user', select: ' -password '});
+
+  if (!session) {
+      return Promise.reject(new DuciError(errorTypes.unauthorised.invalidToken));
+  }
+
+  let dateNow = new Date();
+  let dateExpiry = new Date(session.expiry);
+
+  if (dateExpiry <= dateNow) {
+      await AuthController.logout(req);
+      return Promise.reject(new DuciError(errorTypes.unauthorised.invalidToken));
+  }
+
+  try {
+      jwt.verify(token, SECRET);
+      // If we have no errors, return the partial user from our session query above
+      return session.user;
+  } catch (e) {
+      // log user out if the dateExpiry check passed but token check failed
+      await AuthController.logout(req);
+      return Promise.reject(new DuciError(errorTypes.unauthorised.invalidToken, null, e));
+  }
+}
