ea4fb2a6a522b6b2089ec4632ce3c58aaae63273
Henrique Pureza <henrique.pureza00@gmail.com>
2019-02-27 14:05:01 +1100

    Implemented async handler in router

--- backend/routes/v1.0/auth.route.js
+++ backend/routes/v1.0/auth.route.js
@@ -1,13 +1,25 @@
 let middleware = require('../../middleware/JWTmiddleware');
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('express-async-handler');
 
 import * as authController from '../../controllers/auth.controller';
 
 const controllerHandler = require('../../util/controllerHandler');
 const c = controllerHandler;
 
-router.post('/login', c(authController.login, (req, res, next) => [req])); 
+// router.post('/login', c(authController.login, (req, res, next) => [req])); 
+
+router.post('/login',  
+asyncHandler(async (req, res, next) => {
+  try {
+      const loginDetails = await authController.login(req);
+
+      return res.ok(loginDetails);
+  } catch (e) {
+      return res.handleError(e);
+  }
+}));
 
 router.post('/forgot-password', c(authController.forgotPassword, (req, res, next) => [req])); 
 
